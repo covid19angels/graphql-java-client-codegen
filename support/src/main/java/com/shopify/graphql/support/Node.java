57c3a5e9d5bc616b124ec637bdc37b231179ca7b
陈朵拉 <conan8chan@yahoo.com>
2020-09-11 10:38:35 +0800

    TextBlock

--- support/src/main/java/com/shopify/graphql/support/Node.java
+++ support/src/main/java/com/shopify/graphql/support/Node.java
@@ -2,7 +2,5 @@ package com.shopify.graphql.support;
 
 // Helpful to have this common base class for Relay-compatible schemas. Unused otherwise.
 public interface Node {
-    String getGraphQlTypeName();
-
     ID getId();
 }
